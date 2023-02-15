d60293a2648cdfc220ad44b1aa80603b8297360c
Newtayo <akandeabdul@gmail.com>
2023-02-15 22:46:26 +0100

    Refactored using classes

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
+/* eslint-disable max-classes-per-file */
 const addbutton = document.querySelector('.btn');
 const addingbook = document.querySelector('.addingbook');
 const ContainerSection = document.querySelector('.container');
-const bookholder = {
-  title: 'Don Quixote',
-  author: 'Miguel de Cervantes',
-};
+
 let bookcollection;
 
-if (((localStorage.getItem('links') === null) || ((localStorage.getItem('links'))))) {
+class Books {
+  constructor(title, author, id) {
+    this.title = title;
+    this.author = author;
+    this.id = id;
+  }
+}
+
+if (localStorage.getItem('links') === null) {
   bookcollection = JSON.parse(localStorage.getItem('links'));
 } else {
-  bookcollection = [{
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien',
-    id: '1',
-  },
-  {
-    title: 'Don Quixote',
-    author: 'Miguel de Cervantes',
-    id: '2',
-  },
-  ];
+  const book1 = new Books('Lord of the Rings', 'J.R.R. Tolkien', '1');
+  const book2 = new Books('Don Quixote', 'Miguel de Cervantes', '2');
+  bookcollection = [book1, book2];
 }
 // dynamically load the page
 function updatingstorage() {
   localStorage.setItem('links', JSON.stringify(bookcollection));
 }
 
-function bookremoval(id) {
-  const filtered = bookcollection.filter((elem) => elem.id !== id);
-  bookcollection = filtered;
-  updatingstorage();
+class Activity {
+  addbook() {
+    const newBook = new Books();
+    newBook.title = addingbook.elements.title.value;
+    newBook.author = addingbook.elements.author.value;
+    newBook.id = `${new Date().getTime()}`;
+    bookcollection.push(newBook);
+
+    updatingstorage();
+
+    return this;
+  }
+
+  bookremoval(id) {
+    const filtered = bookcollection.filter((elem) => elem.id !== id);
+    bookcollection = filtered;
+    updatingstorage();
+    return this;
+  }
 }
 
+const newAction = new Activity();
 function bookArrangement(data) {
   const bookdetails = document.createElement('div');
   bookdetails.className = 'bookdetails';
@@ -44,7 +58,7 @@ function bookArrangement(data) {
   ContainerSection.append(bookdetails);
   const removeBtn = document.getElementById(`${data.id}`);
   removeBtn.addEventListener('click', (e) => {
-    bookremoval(e.target.id);
+    newAction.bookremoval(e.target.id);
     ContainerSection.innerHTML = '';
     bookcollection.forEach((book) => {
       bookArrangement(book);
@@ -71,12 +85,6 @@ display();
 
 addbutton.addEventListener('click', (event) => {
   event.preventDefault();
-  const newBook = Object.create(bookholder);
-  newBook.title = addingbook.elements.title.value;
-  newBook.author = addingbook.elements.author.value;
-  newBook.id = `${new Date().getTime()}`;
-  bookcollection.push(newBook);
-  bookArrangement(newBook);
-  updatingstorage();
-  bookremoval();
+  newAction.addbook();
+  display();
 });
